181ed463855f40c46f1da505b4d2bec2b7b686a9
Keeri <keeric25@gmail.com>
2026-07-10 16:42:55 +0000

    cat

---
+++ cat.py
@@ -0,0 +1,52 @@
+#print("meow")
+#print("meow")
+#print("meow")
+
+# Make a loop
+
+#i = 3
+#while i != 0:
+    #print("meow")
+
+# The code above makes an infinite loop
+# Fix the loop
+#i = 3
+#while i != 0:
+    #print("meow")
+    #i = i -1
+
+# Improve code further
+#i = 1
+#while i <= 3:
+    #print("meow")
+    #i = i +1
+
+#Improve further code to start counting with zero
+#i = 0
+#while i <3:
+    #print("meow")
+    #i += 1
+
+# A for loop iterates thru a list of items.
+# Modify code again for example
+#for i in [0,1,2]:
+    #print("meow")
+# Code above is much cleaner but still can be improved
+#for i in range(3):
+    #print("meow")
+
+# i is a place to store the number of the iteration loop, never used for another purpose
+# Python, if such a variable doesnt have any significance in our code,
+# we can simply represent this variable as a single underscore_.
+
+
+# Modify code further
+#for _ in range(3):
+    #print("meow")
+
+# Can still be further improved
+#print("meow" *3)
+
+
+# Output is meowmeowmeow. fix with a line break at the end of each meow
+print("meow\n" * 3, end="")
