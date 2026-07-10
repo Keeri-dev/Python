f102a06452c16106dc340e6979e25730c71d40ef
Keeri <keeric25@gmail.com>
2026-07-10 16:39:44 +0000

    args and kwargs

---
+++ args_and_kwargs.py
@@ -0,0 +1,33 @@
+###### args and kwargs ######
+
+###### Recall the print documentation we looked at earlier in this course:
+
+#print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+
+#### args are postional arguments, such as those we provide to print like print("Hello", "World").
+### kwargs are named arguments, or "keyword arguments", such as those we provide to print like print(end="").
+
+##### As we see in the prototype for the print function above, we can tell our function to expect a presently unknown number postional arguments.
+#### We can also tell it to expect a presently unknown number of keyword arguments.
+
+### In the text editor window, code as follows:
+
+#def f(*args, **kwargs):
+#    print("Positional:", args)
+#
+#
+#f(100, 50, 25)
+
+##### Notice hwo executing this code will be printed as postional arguments.
+
+##### We can even pass in named arguments. In the text editor window, code as follows:
+
+#def f(*args, **kwargs):
+#    print("Named:", kwargs)
+#
+#
+#f(galleons=100, sickles=50, knuts=25)
+
+##### Notice how the named values are provided in the form of a dictionary.
+
+#### Thinking about the print function above, you can see how *objects takes any number of positional arguments.
