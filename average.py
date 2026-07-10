a18294a9414aabffbcf42cb1b6996c921d236a63
Keeri <keeric25@gmail.com>
2026-07-10 16:40:34 +0000

    average

---
+++ average.py
@@ -0,0 +1,18 @@
+###### Statistics ######
+
+### Python comes with built-in statistics library.
+## How might we use this module?
+# mean is a function of this library that is quite useful.
+
+# In your terminal window, type code average.py.
+
+# In the text editor window, modify your code as follows:
+
+#import statistics
+#
+#print(statistics.mean([100, 90]))
+
+#### Notice that we imported a different library called statistics.
+### The mean function takes a list of values. This will print the average of these values.
+## In your terminal window, type python average.py.
+# Consider the possibilities of using the statistics module in your own programs.
