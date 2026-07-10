1fe65e0bcdd57164682854cc081c8c6e5fa47ad4
Keeri <keeric25@gmail.com>
2026-07-10 16:53:00 +0000

    hello

---
+++ hello.py
@@ -0,0 +1,10 @@
+## Exceptions
+# Create new a file, type as follows with the intentional errors included):
+
+#print("hello, world)
+
+# Notice we intentionally left out a quatation mark.
+
+### Running python hello.py in out terminal window, an error is outputted.
+## The compiler states that it is a "syntax error."
+# Syntax errors are those that require you to double-check that you typed in your code correction.
