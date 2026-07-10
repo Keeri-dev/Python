44073adb7ae8f245c68a85e56c7935f996141151
Keeri <keeric25@gmail.com>
2026-07-10 16:56:39 +0000

    list

---
+++ list_comprehensions.py
@@ -0,0 +1,23 @@
+###### List Comprehensions ######
+
+##### List comprehensions allow you to create a list onthe fly in one elegant one=liner.
+
+#### We can implement this in our code as follows:
+
+#def main():
+#    yell("This", "is", "CS50")
+#
+#
+#def yell(*words):
+#    uppercased = [arg.upper() for arg in words]
+#    print(*uppercased)
+#
+#
+#if __name__ == "__main__":
+#    main()
+
+##### Notice how instead of using map, we write Python expression within square brackets. For each argument, .upper is applied to it.
+#### Taking this concept further, lets pivot toward another program.
+
+### In the text editor window, type code gryffindors.py
+## SEE GRYFFINDORs.PY FOR CONTINUATION.
